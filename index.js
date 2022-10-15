710dffa1fe62dbea9e2778c8ba343d1f15dff1d2
Ranganath Swamy <70009494+Ranganath0108@users.noreply.github.com>
2022-10-15 20:57:09 +0530

    Session TimeOut to 8min

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var context = document.getElementById("puzzle");
 var canvas = context.getContext("2d");
 
 
-const timeOutMin = 10
+const timeOutMin = 8
 const timeOutInSec = timeOutMin * 60 * 1000
 
 var img = new Image();
